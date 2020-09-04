6cba3f0fdc7764ade4154d302210fe592614dd4b
marialenaa <marialena@wanadoo.fr>
2020-09-04 12:33:45 +0200

    rebase finish

---
+++ reactapp/build/precache-manifest.e6856766ace7601eff715f72ca05d938.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9b13f433ff1a7e0c390cf01708c8f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "594f00b0a41329f208d1",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bf9edbaabd2d11def963",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "594f00b0a41329f208d1",
+    "url": "/static/js/2.c45fce19.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.c45fce19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9edbaabd2d11def963",
+    "url": "/static/js/main.800a3fda.chunk.js"
+  },
+  {
+    "revision": "d26d2baee1ca38baee25",
+    "url": "/static/js/runtime-main.d7015fec.js"
+  }
+]);
